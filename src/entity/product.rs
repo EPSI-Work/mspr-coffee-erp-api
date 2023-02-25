df31e5bf001f649df9a7683a2560022edef636be
aimericsr <52284219+aimericsr@users.noreply.github.com>
2023-02-25 13:01:42 +0100

    remove null field for products

--- src/entity/product.rs
+++ src/entity/product.rs
@@ -1,8 +1,10 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 use std::cmp::Ordering;
 use uuid::Uuid;
 
+#[skip_serializing_none]
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Product {
     pub id: Uuid,
